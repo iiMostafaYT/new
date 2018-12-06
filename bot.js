8c90058d025683dec33a30b8adebc8437fd7bfc8
iiMostafaYT <43861336+iiMostafaYT@users.noreply.github.com>
2018-12-06 22:15:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,37 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require('discord.js'); //Z Bot
+
+const Util = require('discord.js');
+
+const GOOGLE_API_KEY = "AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8"
+
+const getYoutubeID = require('get-youtube-id');
+
+const Canvas = require("canvas");
+
+const jimp = require("jimp");
+
+const convert = require("hh-mm-ss");
+
+const dateFormat = require('dateformat');
+
+const fetchVideoInfo = require('youtube-info');
+
+const YouTube = require('simple-youtube-api');
+
+const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4"; 
+
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+
+const ytdl = require('ytdl-core');
+
+const fs = require('fs');
+
+const gif = require("gif-search");
+
+const moment = require('moment');
+
+const client = new Discord.Client({disableEveryone: true});
+
 const prefix = '$'
 
 client.on('ready', () => {
