ca335ecff7e834ac46acaaf32b7baa1bf5324ec2
iiMostafaYT <43861336+iiMostafaYT@users.noreply.github.com>
2018-12-19 05:21:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4783,4 +4783,49 @@ client.on('voiceStateUpdate', (voiceOld, voiceNew) => {
     }
 });
 
+client.on('message', message => {
+     var prefix = "$"
+if (message.content.startsWith(prefix + "uptime")) {
+    let uptime = client.uptime;
+ 
+    let days = 0;
+    let hours = 0;
+    let minutes = 0;
+    let seconds = 0;
+    let notCompleted = true;
+ 
+    while (notCompleted) {
+ 
+        if (uptime >= 8.64e+7) {
+ 
+            days++;
+            uptime -= 8.64e+7;
+ 
+        } else if (uptime >= 3.6e+6) {
+ 
+            hours++;
+            uptime -= 3.6e+6;
+ 
+        } else if (uptime >= 60000) {
+ 
+            minutes++;
+            uptime -= 60000;
+ 
+        } else if (uptime >= 1000) {
+            seconds++;
+            uptime -= 1000;
+ 
+        }
+ 
+        if (uptime < 1000)  notCompleted = false;
+ 
+    }
+ 
+    message.channel.send("`" + `${days} days, ${hours} hrs, ${minutes} min , ${seconds} sec` + "`");
+ 
+ 
+}
+});
+
+
         client.login(process.env.BOT_TOKEN);
