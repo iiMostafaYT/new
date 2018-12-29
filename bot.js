5915380bfaebb4476113c2e479f6a82ec678e8db
xMasTeR <43861336+iiMostafaYT@users.noreply.github.com>
2018-12-29 12:25:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2786,6 +2786,33 @@ message.channel.send(`:white_check_mark: ${user} has been muted ! :zipper_mouth:
 
 });
 
+client.on('message', message => {//new msg event
+if(!message.channel.guild) return;
+  if(message.content.startsWith(prefix + 'rainbow')) {//to create the rainbow role
+	  let role = message.guild.roles.find('name', 'Rainbow bot.')
+    if(role) return message.channel.send(`This Step Already Completed !`)//if the role already created return with this msg
+  //start of create role 
+  if(!role){
+    rainbow =  message.guild.createRole({
+   name: "Rainbow",//the role will create name
+   color: "#000000",//the default color
+   permissions:[]//the permissions
+ //end of create role
+})
 
+}
+message.channel.send('Done The Rainbow Role Setup Has Been Completed')//if the step completed
+}})
+
+client.on('ready', () => {//new ready event
+  setInterval(function(){
+      client.guilds.forEach(g => {
+                  var role = g.roles.find('name', 'Rainbow');//rainbow role name
+                  if (role) {
+                      role.edit({color : "RANDOM"});
+                  };
+      });
+  }, 5000);//the rainbow time
+})
 
         client.login(process.env.BOT_TOKEN);
